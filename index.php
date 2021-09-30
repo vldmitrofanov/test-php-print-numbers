f496b531b98e6bdb1e6ed2d83978374bdaf9eca5
Vladimir Mitrofanov <shirker2006@gmail.com>
2021-09-30 17:39:32 -0400

    styles added

--- index.php
+++ index.php
@@ -1,63 +1,90 @@
 <!DOCTYPE html>
 <html>
 
+<head>
+    <style>
+        body {
+            font-family: sans-serif;
+        }
+
+        * {
+            box-sizing: border-box;
+        }
+        .box{
+            margin: 10vh auto 0;
+            width: 400px;
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+            border-radius: 5px;
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+        }
+        .results{
+            margin-top: 20px;
+        }
+    </style>
+</head>
+
 <body>
     <div id="main">
-        <h1>Print words from dialpad</h1>
-        <p>Please enter a number from 1 to 1000.</p>
-        <form method="post">
-            <input type="number" name="number" placeholder="Input a Number" />
-            <button type="submit">Print letters</button>
-        </form>
+        <div class="box">
+            <h1>Print words from dialpad</h1>
+            <p>Please enter a number from 1 to 1000.</p>
+            <form method="post">
+                <input type="number" name="number" placeholder="Input a Number" />
+                <button type="submit">Print letters</button>
+            </form>
 
-        <?php
+            <div class="results">
+                <?php
 
-        $hashTable = [
-            0 => '',
-            1 => '',
-            2 => 'ABC',
-            3 => 'DEF',
-            4 => 'GHI',
-            5 => 'JKL',
-            6 => 'MNO',
-            7 => 'PQRS',
-            8 => 'TUV',
-            9 => 'WXYZ'
-        ];
+                $hashTable = [
+                    0 => '',
+                    1 => '',
+                    2 => 'ABC',
+                    3 => 'DEF',
+                    4 => 'GHI',
+                    5 => 'JKL',
+                    6 => 'MNO',
+                    7 => 'PQRS',
+                    8 => 'TUV',
+                    9 => 'WXYZ'
+                ];
 
-        function printWordsUtil($number = [], int $curr_digit, $output = [], int $n)
-        {
-            global $hashTable;
-            if ($curr_digit == $n) {
-                printf("%s ", $output);
-                return;
-            }
+                function printWordsUtil($number = [], int $curr_digit, $output = [], int $n)
+                {
+                    global $hashTable;
+                    if ($curr_digit == $n) {
+                        printf("%s ", $output);
+                        return;
+                    }
 
-            for ($i = 0; $i < strlen($hashTable[$number[$curr_digit]]); $i++) {
-                $output[$curr_digit] = $hashTable[$number[$curr_digit]][$i];
-                printWordsUtil($number, $curr_digit + 1, $output, $n);
-                if ($number[$curr_digit] == 0 || $number[$curr_digit] == 1) return;
-            }
-        }
+                    for ($i = 0; $i < strlen($hashTable[$number[$curr_digit]]); $i++) {
+                        $output[$curr_digit] = $hashTable[$number[$curr_digit]][$i];
+                        printWordsUtil($number, $curr_digit + 1, $output, $n);
+                        if ($number[$curr_digit] == 0 || $number[$curr_digit] == 1) return;
+                    }
+                }
 
-        function printWords($input)
-        {
-            $input_array = str_split($input);
-            $result = '';
-            printWordsUtil($input_array, 0, $result, sizeof($input_array));
-        }
+                function printWords($input)
+                {
+                    $input_array = str_split($input);
+                    $result = '';
+                    printWordsUtil($input_array, 0, $result, sizeof($input_array));
+                }
 
-        if ($_POST['number']) {
-            $num = (int) $_POST['number'];
-            if ($num < 0 || $num > 1000) {
-                echo "<p style='color: red'>Invalid number!</p>";
-            } else {
-                printWords($num);
-            }
-        }
+                if ($_POST['number']) {
+                    $num = (int) $_POST['number'];
+                    if ($num < 0 || $num > 1000) {
+                        echo "<p style='color: red'>Invalid number!</p>";
+                    } else {
+                        printWords($num);
+                    }
+                }
 
-        ?>
-
+                ?>
+            </div>
+        </div>
     </div>
 </body>
 
